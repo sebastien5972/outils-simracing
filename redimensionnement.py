f51ba55ae908b79b71db55c2b1a99dfd7cb7c752
explorer1200 <seb.picxels.2@gmail.com>
2023-06-12 21:49:38 +0200

    maj nom de fichier

--- redimensionnement.py
+++ redimensionnement.py
@@ -8,6 +8,8 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
+nom_dds = input('tape le nom du fichier DDS: ')
+
 file_path = filedialog.askopenfilename()
 print(file_path )
 
@@ -21,16 +23,16 @@ def redimensionner (name_dds):
     img = Image.open(file_path)
 
     img_1024 = img.resize((1024, 576))
-    img_1024.save(file_save_directory + name_dds + ('_1024x576.png'))
+    img_1024.save(file_save_directory + '/' + name_dds + ('_1024x576.png'))
 
     img_512 = img.resize((512, 288))
-    img_512.save(file_save_directory + name_dds + ('_512x288.png'))
+    img_512.save(file_save_directory + '/' + name_dds + ('_512x288.png'))
 
     img_256 = img.resize((256, 144))
-    img_256.save(file_save_directory + name_dds + ('_256x144.png'))
+    img_256.save(file_save_directory + '/' + name_dds + ('_256x144.png'))
 
     img_128 = img.resize((128, 72))
-    img_128.save(file_save_directory + name_dds + ('_128x72.png'))
+    img_128.save(file_save_directory + '/' + name_dds + ('_128x72.png'))
 
 
-redimensionner('/alt_P992CUP')
+redimensionner(nom_dds)
