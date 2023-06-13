c46f7990f754002b32ca2a85a50c6fa8f3fb292c
sebastien5972 <sebastien.pauvert@inetum.com>
2023-06-13 09:13:20 +0200

    maj nom de fichier -icon-

--- redimensionnement.py
+++ redimensionnement.py
@@ -23,16 +23,16 @@ def redimensionner (name_dds):
     img = Image.open(file_path)
 
     img_1024 = img.resize((1024, 576))
-    img_1024.save(file_save_directory + '/' + name_dds + ('_1024x576.png'))
+    img_1024.save(file_save_directory + '/' + name_dds + ('-icon-1024x576.png'))
 
     img_512 = img.resize((512, 288))
-    img_512.save(file_save_directory + '/' + name_dds + ('_512x288.png'))
+    img_512.save(file_save_directory + '/' + name_dds + ('-icon-512x288.png'))
 
     img_256 = img.resize((256, 144))
-    img_256.save(file_save_directory + '/' + name_dds + ('_256x144.png'))
+    img_256.save(file_save_directory + '/' + name_dds + ('-icon-256x144.png'))
 
     img_128 = img.resize((128, 72))
-    img_128.save(file_save_directory + '/' + name_dds + ('_128x72.png'))
+    img_128.save(file_save_directory + '/' + name_dds + ('-icon-128x72.png'))
 
 
 redimensionner(nom_dds)
